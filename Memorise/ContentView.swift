a4babb0d345f4f71f6a099d5852c14c8e7ff971c
ArigatouGozaimaster <modelsine@gmail.com>
2025-03-02 15:38:25 +1100

    Lesson 1: initial commit

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content:  {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                               .fill(.white)
+                            RoundedRectangle(cornerRadius: 12)
+                                .strokeBorder(lineWidth: 2)
+                                .foregroundStyle(.orange)
+                            Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.orange)
+            }
+        })
     }
 }
 
